27481de572a482c899ccd4a72dc0a36cfe275399
etienno <etienne.laverdiere@gmail.com>
2014-07-07 21:50:28 -0400

    Commit to etienno

--- prodobs/prodobs-neo4j/src/main/java/ca/digitaltango/prodobs/graph/repository/SocioService.java
+++ prodobs/prodobs-neo4j/src/main/java/ca/digitaltango/prodobs/graph/repository/SocioService.java
@@ -10,16 +10,27 @@ public interface SocioService extends GraphRepository<Circle> {
 
 	/*
 	 * En tant que membre d'un cercle je veux prendre une décision par
-	 * consentement 
+	 * consentement
 	 * 
-	 * En tant que membre d'un cercle, je veux élire un
-	 * représentant pour une durée déterminée 
+	 * En tant que membre d'un cercle, je veux élire un représentant pour une
+	 * durée déterminée
 	 * 
-	 * En tant que membre d'un cercle je
-	 * veux créer un sous cercle avec un responsable nommé lors d'une prise de
-	 * décision spécifique 
+	 * En tant que membre d'un cercle je veux créer un sous cercle avec un
+	 * responsable nommé lors d'une prise de décision spécifique
 	 * 
-	 * En tant que représentant élu d'un cercle donné, je
-	 * veux participer à toutes les réunions de décision du cercle supérieur
+	 * En tant que représentant élu d'un cercle donné, je veux participer à
+	 * toutes les réunions de décision du cercle supérieur
+	 * 
+	 * 
+	 * 
+	 * le cercle comme lieu de prise de décisions,
+	 * 
+	 * le consentement comme mode de prise de décisions
+	 * 
+	 * le double lien comme instrument de communication entre les niveaux
+	 * hiérarchiques.
+	 * 
+	 * l’élection par consentement pour affecter les membres de l’unité de
+	 * travail dans leurs fonctions.
 	 */
 }
